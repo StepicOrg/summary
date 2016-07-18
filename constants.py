14bf5882e3f96532eefcc052fb70f630f05573d0
Kostya Charkin <93kostya@gmail.com>
2016-07-18 16:43:53 +0300

    fix constant naming

--- constants.py
+++ constants.py
@@ -23,13 +23,13 @@ IMG_NAME_TEMPLATE = '{number}.png'
 DIFFS_PNG_NAME = 'diffs.png'
 
 # YandexSpeechKit
-from secret import key, UUID
+from secret import KEY, UUID
 
 URL = 'https://asr.yandex.net/asr_xml'
-topic = 'notes'
-lang = 'ru-RU'
+TOPIC = 'notes'
+LANG = 'ru-RU'
 REQUEST_URL = '{url}?uuid={UUID}&key={key}&topic={topic}&lang={lang}'.format(url=URL,
                                                                              UUID=UUID,
-                                                                             key=key,
-                                                                             topic=topic,
-                                                                             lang=lang)
+                                                                             key=KEY,
+                                                                             topic=TOPIC,
+                                                                             lang=LANG)
