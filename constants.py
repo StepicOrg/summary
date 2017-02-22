e5283a55d56d5cec1e9fb9e212be166d3e947290
Kostya Charkin <93kostya@gmail.com>
2017-02-22 17:13:40 +0300

    add converting to <math>

--- constants.py
+++ constants.py
@@ -87,3 +87,8 @@ STEP_PAGE_TEXT_TEMPLATE = textwrap.dedent("""\
 STEP_PAGE_SUMMARY_TEMPLATE = 'Create page for step id={id}'
 
 EMPTY_STEP_TEXT = 'Empty step'
+
+SINGLE_DOLLAR_TO_MATH_PATTERN = r'([^\\\$]|^)(?:\$)((?:[^\\\$]|\\.)+)(?:\$)($|[^\$])'
+SINGLE_DOLLAR_TO_MATH_REPLACE = r'\1<math>\2</math>\3'
+DOUBLE_DOLLAR_TO_MATH_PATTERN = r'([^\\\$]|^)(?:\$\$)((?:[^\\\$]|\\.)+)(?:\$\$)($|[^\$])'
+DOUBLE_DOLLAR_TO_MATH_REPLACE = r'\1\n\n<math>\2</math>\n\n\3'
